7f25ecf86310b5a584bef2b9b9cdce0c61c07ed9
Sapunov <ilsapunov@gmail.com>
2024-04-22 15:56:02 +0300

    rewrite store with middleware

--- src/store/index.js
+++ src/store/index.js
@@ -1,25 +1,36 @@
-import { legacy_createStore as createStore, combineReducers } from 'redux';
+import { legacy_createStore as createStore, combineReducers, compose, applyMiddleware } from 'redux';
 import heroes from '../reducers/heroes';
 import filters from '../reducers/filters';
 
-const enhancer = (createStore) => (...args) => {
-    const store = createStore(...args);
-
-    const oldDispatch = store.dispatch;
-
-    store.dispatch = (action) => {
-        if (typeof action === 'string') {
-            return oldDispatch({
-                type: action
-            })
-        }
-        return oldDispatch(action)
+const stringMiddleware = () =>(next) => (action) =>{
+    if (typeof action === 'string') {
+        return next({
+            type: action
+        })
     }
-    return store;
-}
+    return next(action)
+} 
 
-const store = createStore(combineReducers({heroes, filters}), enhancer);
+// const enhancer = (createStore) => (...args) => {
+//     const store = createStore(...args);
+
+//     const oldDispatch = store.dispatch;
+
+//     store.dispatch = (action) => {
+//         if (typeof action === 'string') {
+//             return oldDispatch({
+//                 type: action
+//             })
+//         }
+//         return oldDispatch(action)
+//     }
+//     return store;
+// }
+
+const store = createStore(
+                    combineReducers({heroes, filters}), 
+                    compose(applyMiddleware(stringMiddleware), 
+                    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+                );
 
 export default store;
-
-//window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
