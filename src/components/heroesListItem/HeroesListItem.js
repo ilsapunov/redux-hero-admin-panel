99be036cd8385f5793ebf7bda88175b4e73e8ef1
Sapunov <ilsapunov@gmail.com>
2024-04-22 12:52:50 +0300

    add functionality

--- src/components/heroesListItem/HeroesListItem.js
+++ src/components/heroesListItem/HeroesListItem.js
@@ -1,6 +1,6 @@
 import img from '../../assets/unknown_hero.jpg'
 
-const HeroesListItem = ({name, description, element}) => {
+const HeroesListItem = ({name, description, element, onDelete}) => {
 
     let elementClassName;
 
@@ -33,7 +33,8 @@ const HeroesListItem = ({name, description, element}) => {
                 <h3 className="card-title">{name}</h3>
                 <p className="card-text">{description}</p>
             </div>
-            <span className="position-absolute top-0 start-100 translate-middle badge border rounded-pill bg-light">
+            <span onClick={onDelete}
+            className="position-absolute top-0 start-100 translate-middle badge border rounded-pill bg-light">
                 <button type="button" className="btn-close btn-close" aria-label="Close"></button>
             </span>
         </li>
