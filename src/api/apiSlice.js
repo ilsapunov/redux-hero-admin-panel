b1e72d987cf583849778968c4bea29640e1862d6
Sapunov <ilsapunov@gmail.com>
2024-05-17 14:06:35 +0300

    update db link

--- src/api/apiSlice.js
+++ src/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const apiSlice = createApi({
     reducerPath: 'api',
-    baseQuery: fetchBaseQuery({baseUrl: 'http://localhost:3001'}),
+    baseQuery: fetchBaseQuery({baseUrl: 'https://my-json-server.typicode.com/ilsapunov/redux-hero-admin-panel'}),
     tagTypes: ['Heroes'],
     endpoints: builder => ({
         getHeroes: builder.query({
